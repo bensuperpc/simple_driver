c3350b715b0ed0b700212f0e29e3dc9e435b9bb5
Bensuperpc <bensuperpc@gmail.com>
2023-01-31 20:56:19 +0100

    Update code and readme
    Signed-off-by: Bensuperpc <bensuperpc@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# simple_driver
+# simple_kernel_driver
+
+A simple kernel driver for Linux.
+
+## Requirements
+
+- Linux kernel headers
+- GCC
+- Make
+
+## Build
+
+```sh
+make
+```
+
+## Install (Load)
+
+```sh
+sudo make load
+```
+
+## Uninstall (Unload)
+
+```sh
+sudo make unload
+```
+
+## Clean
+
+```sh
+make clean
+```
+
+## License
+
+[GPLv3](LICENSE)
+
+## Source
+
+- [linux-kernel-labs](https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html)
+- [embeddedguruji](http://embeddedguruji.blogspot.com/2019/01/linux-character-driver-creating.html)
+- [apriorit](https://www.apriorit.com/dev-blog/195-simple-driver-for-linux-os)
+- [sysprog21](https://sysprog21.github.io/lkmpg/)
--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 /*
-More info:
-
-https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html#waiting-queues
-http://embeddedguruji.blogspot.com/2019/01/linux-character-driver-creating.html
-https://www.apriorit.com/dev-blog/195-simple-driver-for-linux-os
-https://sysprog21.github.io/lkmpg/
-
+    * Simple Linux driver
+    * 
+    * This is a simple Linux driver that can be used as a template for other drivers.
+    * Multiple devices can be created with this driver on /dev/device_driverX where X is the minor number.
+    * Code is written in C89 and tested with Linux 5.15.
+    * 
+    * This driver is based on the following sources:
+    * - https://linux-kernel-labs.github.io/refs/heads/master/labs/device_drivers.html
+    * - http://embeddedguruji.blogspot.com/2019/01/linux-character-driver-creating.html
+    * - https://www.apriorit.com/dev-blog/195-simple-driver-for-linux-os
+    * - https://sysprog21.github.io/lkmpg/
 */
 
 #include <linux/version.h> /* LINUX_VERSION_CODE, KERNEL_VERSION */
 #include <linux/compiler.h> /* __must_check */
 
 #include <linux/kernel.h> /* printk, pr_err, pr_debug */
-#include <linux/init.h>
+#include <linux/init.h> /* __init, __exit */
 #include <linux/module.h> /* THIS_MODULE */
-#include <linux/fs.h>
+#include <linux/fs.h> /* file_operations */
 #include <linux/cdev.h> /* cdev */
-#include <asm/ioctl.h>
+#include <asm/ioctl.h> /* ioctl */
 #include <linux/slab.h> /* kmalloc / kfree */
 #include <linux/sched.h> /* wait_* */
 
@@ -37,30 +41,38 @@ MODULE_INFO(name, "simple_linux_driver");
 MODULE_INFO(OS, "Linux");
 MODULE_VERSION("1.0.0");
 
-struct my_device_data
+/* Device data structure */
+struct device_data
 {
+    /* Char device */
     struct cdev cdev;
-    // Device data
+    /* Device data */
     char *buffer;
     ssize_t size;
 };
 
-struct my_device_data devs[NBR_MINORS];
+/* Store device data */
+static struct device_data devs[NBR_MINORS];
 
+/* Store device number */
 static dev_t device_number;
-struct class *device_class;
 
+/* Store device class */
+static struct class *device_class;
+
+/* When the device is opened */
 static int open_device(struct inode *inode, struct file *file)
 {
-    struct my_device_data *my_data = &devs[iminor(inode)];
+    struct device_data *data = &devs[iminor(inode)];
 
     pr_debug("[open_device]\n");
 
-    file->private_data = my_data;
+    file->private_data = data;
 
     return 0;
 }
 
+/* When the device is closed */
 static int close_device(struct inode *inode, struct file *file)
 {
     pr_debug("[close_device]\n");
@@ -68,11 +80,12 @@ static int close_device(struct inode *inode, struct file *file)
     return 0;
 }
 
+/* When the device is read */
 static ssize_t read_device(struct file *file, char __user *user_buffer, size_t size, loff_t *offset)
 {
-    struct my_device_data *my_data =
-        (struct my_device_data *)file->private_data;
-    ssize_t len = min((ssize_t)(my_data->size - *offset), (ssize_t)size);
+    struct device_data *data =
+        (struct device_data *)file->private_data;
+    ssize_t len = min((ssize_t)(data->size - *offset), (ssize_t)size);
 
     pr_debug("[read_device]\n");
 
@@ -81,7 +94,8 @@ static ssize_t read_device(struct file *file, char __user *user_buffer, size_t s
         return 0;
     }
 
-    if (copy_to_user(user_buffer, my_data->buffer + *offset, len))
+    /* Copy data from kernel space to user space */
+    if (copy_to_user(user_buffer, data->buffer + *offset, len))
     {
         pr_err("copy_to_user error");
         return -EFAULT;
@@ -91,10 +105,11 @@ static ssize_t read_device(struct file *file, char __user *user_buffer, size_t s
     return len;
 }
 
+/* When the device is written */
 static ssize_t write_device(struct file *file, const char __user *user_buffer, size_t size, loff_t *offset)
 {
-    struct my_device_data *my_data = (struct my_device_data *)file->private_data;
-    ssize_t len = min((ssize_t)(my_data->size - *offset), (ssize_t)size);
+    struct device_data *data = (struct device_data *)file->private_data;
+    ssize_t len = min((ssize_t)(data->size - *offset), (ssize_t)size);
 
     pr_debug("[write_device]\n");
     if (len <= 0)
@@ -102,16 +117,17 @@ static ssize_t write_device(struct file *file, const char __user *user_buffer, s
         return 0;
     }
     
-    if (size != my_data->size) {
-        if(my_data->buffer = krealloc(my_data->buffer, sizeof(char) * size, GFP_KERNEL | GFP_ATOMIC))
+    if (size != data->size) {
+        if(data->buffer = krealloc(data->buffer, sizeof(char) * size, GFP_KERNEL | GFP_ATOMIC))
         {
             return -ENOMEM;
         }
 
-        my_data->size = size;
+        data->size = size;
     }
 
-    if (copy_from_user(my_data->buffer + *offset, user_buffer, len))
+    /* Copy data from user space to kernel space */
+    if (copy_from_user(data->buffer + *offset, user_buffer, len))
     {
         pr_err("copy_from_user error");
         return -EFAULT;
@@ -121,7 +137,7 @@ static ssize_t write_device(struct file *file, const char __user *user_buffer, s
     return len;
 }
 
-struct file_operations my_fops = {
+static struct file_operations fops = {
     .owner = THIS_MODULE,
     .open = open_device,
     .read = read_device,
@@ -129,35 +145,37 @@ struct file_operations my_fops = {
     .release = close_device,
 };
 
+/* When the module is loaded */
 static int __init register_device(void)
 {
     int i = 0;
     int major = 0;
-    dev_t my_device = 0;
+    dev_t device = 0;
 
     pr_debug("Start initialization\n");
 
-    if (alloc_chrdev_region(&device_number, 0, NBR_MINORS, "my_device_driver"))
+    if (alloc_chrdev_region(&device_number, 0, NBR_MINORS, "device_driver"))
     {
         pr_err("alloc_chrdev_region error");
         return -ENOMEM;
     }
 
-    device_class = class_create(THIS_MODULE, "my_driver_class");
+    device_class = class_create(THIS_MODULE, "driver_class");
     major = MAJOR(device_number);
 
     for (i = 0; i < NBR_MINORS; i++)
     {
-        my_device = MKDEV(major, i);
-        cdev_init(&devs[i].cdev, &my_fops);
+        device = MKDEV(major, i);
+        cdev_init(&devs[i].cdev, &fops);
 
-        if (cdev_add(&devs[i].cdev, my_device, 1))
+        if (cdev_add(&devs[i].cdev, device, 1))
         {
             pr_err("cdev_add error");
             return -ENOMEM;
         }
-        device_create(device_class, NULL, my_device, NULL, "my_device_driver%d", i);
-        pr_info("Device created on /dev/%s%d\n", "my_device_driver", i);
+        
+        device_create(device_class, NULL, device, NULL, "device_driver%d", i);
+        pr_info("Device created on /dev/%s%d\n", "device_driver", i);
 
         devs[i].size = 6;
         devs[i].buffer = kmalloc(sizeof(char) * 6, GFP_KERNEL | GFP_ATOMIC);
@@ -172,11 +190,12 @@ static int __init register_device(void)
     return 0;
 }
 
+/* When the module is unloaded */
 static void __exit unregister_device(void)
 {
     int i = 0;
     int major = 0;
-    dev_t my_device = 0;
+    dev_t device = 0;
 
     pr_debug("Start termination\n");
 
@@ -185,9 +204,9 @@ static void __exit unregister_device(void)
 
     for (i = 0; i < NBR_MINORS; i++)
     {
-        my_device = MKDEV(major, i);
+        device = MKDEV(major, i);
         cdev_del(&devs[i].cdev);
-        device_destroy(device_class, my_device);
+        device_destroy(device_class, device);
 
         kfree(devs[i].buffer);
         devs[i].size = 0;
